c059f03c367ab6c7a6433230ca78569c14a1acb4
Closer <52554717+ShiShc@users.noreply.github.com>
2021-01-31 09:40:45 +0800

    31

--- 1-31/src/MaxQueue.java
+++ 1-31/src/MaxQueue.java
@@ -1,4 +1,6 @@
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @PackageName:PACKAGE_NAME
@@ -8,51 +10,107 @@ import java.util.LinkedList;
 
 public class MaxQueue {
 
-    LinkedList<Integer> A, B;
+    /**
+     * boolean add(E e); 添加队列
+     * E element(); 检索，但不删除，这个队列的头
+     * boolean offer(E e); 插入
+     * E peek() 检索但不删除 返回null
+     * E poll() 检索并删除 返回null
+     * E remove() 检索并删除
+     */
+
+    Queue<Integer> q;
+    Deque<Integer> d;
 
     public MaxQueue() {
-        A = new LinkedList<Integer>();
-        B = new LinkedList<Integer>();
+        q = new LinkedList<Integer>();
+        d = new LinkedList<Integer>();
     }
 
     public int max_value() {
-        if(B.isEmpty()) {
-            return -1;
-        }
-        return B.getLast();
+        return d.isEmpty() ? -1:d.peekFirst();
     }
 
     public void push_back(int value) {
-        A.add(value);
-        if(B.isEmpty() || B.getLast() <= value) {
-            B.add(value);
-        }
+        q.offer(value);
+        while(!d.isEmpty() && d.peekLast() < value)
+            d.pollLast();
+        d.offerLast(value);
     }
 
     public int pop_front() {
-        if(A.isEmpty()) {
-            return -1;
-        }
-        int temp = A.getFirst();
+        if(q.isEmpty()) return -1;
 
-        if(A.removeFirst().equals(B.getFirst())) {
-            B.removeFirst();
-        }
+        if(q.peek().equals(d.peekFirst()))
+            d.pollFirst();
 
-        return temp;
+        return q.poll();
     }
+
+
+
+//    LinkedList<Integer> A, B;
+//
+//    public MaxQueue() {
+//        A = new LinkedList<Integer>();
+//        B = new LinkedList<Integer>();
+//    }
+//
+//    public int max_value() {
+//        if(B.isEmpty()) {
+//            return -1;
+//        }
+//        return B.getLast();
+//    }
+//
+//    public void push_back(int value) {
+//        A.add(value);
+//        if(B.isEmpty() || B.getLast() < value) {
+//            B.add(value);
+//        }
+//    }
+//
+//    public int pop_front() {
+//        if(A.isEmpty()) {
+//            return -1;
+//        }
+//        int temp = A.getFirst();
+//
+//        if(A.removeFirst().equals(B.getFirst())) {
+//            B.removeFirst();
+//        }
+//
+//        return temp;
+//    }
 
     public static void main(String[] args) {
         MaxQueue obj = new MaxQueue();
-        obj.push_back(1);
-        obj.push_back(2);
         System.out.println(obj.max_value());
         System.out.println(obj.pop_front());
         System.out.println(obj.max_value());
+        obj.push_back(46);
+        System.out.println(obj.max_value());
+        System.out.println(obj.pop_front());
+        System.out.println(obj.max_value());
+        System.out.println(obj.pop_front());
+        obj.push_back(868);
+        System.out.println(obj.pop_front());
+        System.out.println(obj.pop_front());
+        System.out.println(obj.pop_front());
+        obj.push_back(525);
+        System.out.println(obj.pop_front());
+        System.out.println(obj.max_value());
+        System.out.println(obj.pop_front());
+        System.out.println(obj.max_value());
+        obj.push_back(123);
+        obj.push_back(646);
+        System.out.println(obj.max_value());
+        obj.push_back(229);
+        System.out.println(obj.max_value());
 
-        MaxQueue obj2 = new MaxQueue();
-        System.out.println(obj2.pop_front());
-        System.out.println(obj2.max_value());
+
+
+
     }
 
 
