a5d8051fa1b7eddb6375290c8b1ad5ae5983974b
Closer <52554717+ShiShc@users.noreply.github.com>
2021-02-03 11:53:53 +0800

    2-3-1

--- 2-2/src/isValid.java
+++ 2-2/src/isValid.java
@@ -10,16 +10,42 @@ import java.util.Stack;
 public class isValid {
 
     public static boolean isValid(String s) {
+        if(s.length() % 2 != 0) {
+            return false;
+        }
+
         Stack<Character> stack = new Stack<>();
+        int needBreak = 0;
 
         for(int i = 0;i < s.length(); ++i) {
             char c = s.charAt(i);
-
-
-
+            if(c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.peek() != '[') {
+                    needBreak = 1;
+                    break;
+                }
+                stack.pop();
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.peek() != '{') {
+                    needBreak = 1;
+                    break;
+                }
+                stack.pop();
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.peek() != '(') {
+                    needBreak = 1;
+                    break;
+                }
+                stack.pop();
             }
         }
 
+        if(needBreak == 1 || !stack.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 
 
---
+++ 2-3/src/BSTIterator.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+/**
+ * @PackageName:PACKAGE_NAME
+ * @Date:2021/2/3, 9:46
+ * @Auther:ShiShc
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) {this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BSTIterator {
+
+    Stack<Integer> stack;
+
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    public int next() {
+
+    }
+
+    public boolean hasNext() {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
