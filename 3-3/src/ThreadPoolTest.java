6aef0a443be9a8b83ea2771b593654da90082027
Closer <52554717+ShiShc@users.noreply.github.com>
2021-03-03 18:47:56 +0800

    3-3
    ThreadPool

---
+++ 3-3/src/ThreadPoolTest.java
@@ -0,0 +1,53 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @PackageName:PACKAGE_NAME
+ * @Date:2021/3/3, 18:27
+ * @Auther:ShiShc
+ */
+
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        /**
+         * 在Java doc中，并不提倡直接使用ThreadPoolExecutor，而是使用Executors类中提供的几个静态方法来创建线程池：
+         * Executors.newCachedThreadPool();
+         * Executors.newSingleThreadExecutor();
+         * Executors.newFixedThreadPool(int);
+         */
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                5, 10, 200, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5)
+        );
+
+        for(int i = 0; i < 15; i ++) {
+            MyTask t = new MyTask(i);
+            executor.execute(t);
+            System.out.println("ThreadPoolNum : " + executor.getPoolSize() + ", QueueNum : " + executor.getQueue().size()
+             + ", CompletedNum : " + executor.getCompletedTaskCount());
+        }
+
+        executor.shutdown();
+    }
+}
+
+class MyTask implements Runnable {
+
+    private int TaskNum;
+
+    MyTask(int num) {
+        this.TaskNum = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running Task " + TaskNum);
+        try {
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Task " + TaskNum + "over!");
+    }
+}
