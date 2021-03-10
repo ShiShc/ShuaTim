4b32e14615941c5cdf0d394fbca9b2a7ee5a52ad
Closer <52554717+ShiShc@users.noreply.github.com>
2021-03-10 23:47:33 +0800

    3.10

---
+++ 3-10/src/NIOClient.java
@@ -0,0 +1,11 @@
+/**
+ * @PackageName:PACKAGE_NAME
+ * @Date:2021/3/10, 9:23
+ * @Auther:ShiShc
+ */
+
+public class NIOClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3-10/src/NIOServer.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @PackageName:PACKAGE_NAME
+ * @Date:2021/3/10, 9:22
+ * @Auther:ShiShc
+ */
+
+public class NIOServer {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+
+        ServerSocketChannel ssChannel = ServerSocketChannel.open();
+        ssChannel.configureBlocking(false);
+        ssChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        ServerSocket serverSocket = ssChannel.socket();
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8888);
+        serverSocket.bind(address);
+
+        String str = "abc";
+        str.intern();
+
+        while(true) {
+            selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> iterable = keys.iterator();
+
+            while(iterable.hasNext()) {
+                SelectionKey key = iterable.next();
+
+                if(key.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
+                    SocketChannel socketChannel1 = serverSocketChannel.accept();
+                    socketChannel1.configureBlocking(false);
+                    socketChannel1.register(selector, SelectionKey.OP_READ);
+                } else if(key.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel)key.channel();
+                }
+            }
+        }
+
+
+
+    }
+}
