51a9f416bff429324d48e1c500d14f18a8ddec16
shishc9 <343141457@qq.com>
2021-04-08 21:22:39 +0800

    1111

---
+++ 4-7/src/Quicksort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+/**
+ * @date:2021/4/8, 11:18
+ * @author:ShiShc
+ */
+
+public class Quicksort {
+    public void f(int[] arr, int left, int right) {
+
+    }
+
+
+    public static void main(String[] args) {
+        Arrays.sort();
+    }
+}
