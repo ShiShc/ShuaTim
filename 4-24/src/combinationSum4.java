08cd44928d3733fc24cc47312fa7ffb7521cc411
shishc9 <343141457@qq.com>
2021-04-24 10:07:50 +0800

    4.24

---
+++ 4-24/src/combinationSum4.java
@@ -0,0 +1,25 @@
+/**
+ * @date: 2021-4-24, 10:00
+ * @author: ShiShc
+ */
+
+public class combinationSum4 {
+    public int f(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 1;i <= target; i++) {
+            for(int num : nums) {
+                if(num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+
+
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
