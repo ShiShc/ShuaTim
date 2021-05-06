116a133d1bbdc768fe9ed25a8bfe65036d0019a4
shishc9 <343141457@qq.com>
2021-05-06 11:24:06 +0800

    5.6

---
+++ 5-6/src/DaysBetweenDates.java
@@ -0,0 +1,23 @@
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * @date: 2021-5-6, 10:56
+ * @author: ShiShc
+ */
+
+public class DaysBetweenDates {
+    public int f(String date1, String date2){
+
+        LocalDate d1 = LocalDate.parse(date1);
+        LocalDate d2 = LocalDate.parse(date2);
+
+        return Math.abs((int) ChronoUnit.DAYS.between(d1, d2));
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new DaysBetweenDates().f("2019-06-29", "2019-06-30"));
+    }
+}
