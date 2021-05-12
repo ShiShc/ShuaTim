2372a1b2203ced76d5b0e7c3eb8aa2fa18789ac3
shishc9 <343141457@qq.com>
2021-05-12 17:28:32 +0800

    Create MyTest.java

---
+++ 5-12/src/MyTest.java
@@ -0,0 +1,10 @@
+/**
+ * @date: 2021-5-12, 15:55
+ * @author: ShiShc
+ */
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println(Long.parseLong("-11456"));
+    }
+}
