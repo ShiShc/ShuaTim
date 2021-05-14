64d77b99b3c1d687a77947066e72b783fd4eaf53
shishc9 <343141457@qq.com>
2021-05-14 09:24:16 +0800

    create IntToRoman.java

---
+++ 5-14/src/IntToRoman.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+/**
+ * @date: 2021-5-14, 09:02
+ * @author: ShiShc
+ */
+
+public class IntToRoman {
+    String[] symbols = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    Integer[] nums = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+    public String f(int num) {
+        StringBuilder result = new StringBuilder();
+        for(int i = nums.length - 1; i >= 0; i--) {
+            while(num >= nums[i]) {
+                num -= nums[i];
+                result.append(symbols[i]);
+            }
+
+            if(num == 0) break;
+        }
+
+        return result.toString();
+    }
+}
