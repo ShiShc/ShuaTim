afca79d6d8c2e1c4bc390ab2d66970d7c2f2fb4f
shishc9 <343141457@qq.com>
2021-05-17 10:23:04 +0800

    Create CoinChange.java

---
+++ 5-17/src/CoinChange.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * @date: 2021-5-17, 09:24
+ * @author: ShiShc
+ */
+
+public class CoinChange {
+    int result = 0;
+
+    public int f(int[] coins, int amount) {
+        Arrays.sort(coins);
+        while(amount != 0) {
+            int temp = help(coins, amount);
+            if(temp == -1) {
+                result = -1;
+                break;
+            } else {
+                amount -= temp;
+            }
+        }
+
+        return result;
+    }
+
+    private int help(int[] coins, int amount) {
+        result ++;
+        for(int right = coins.length - 1; right >= 0; right--) {
+            if(coins[right] <= amount) {
+                return coins[right];
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CoinChange().f(new int[]{1, 2, 5}, 11));
+    }
+
+}
