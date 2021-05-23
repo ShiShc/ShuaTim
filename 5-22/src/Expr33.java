1b5a575e081560eb812a1cc21a71b8577f253bdd
shishc9 <343141457@qq.com>
2021-05-23 10:56:50 +0800

    Create Expr33.java

---
+++ 5-22/src/Expr33.java
@@ -0,0 +1,15 @@
+/**
+ * @date: 2021-5-22, 21:23
+ * @author: ShiShc
+ */
+
+public class Expr33 {
+    public int[] f(int[][] matrix) {
+        return new int[]{};
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
