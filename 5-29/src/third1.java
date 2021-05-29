98a89b158a6a51c30a937afbfa530007c0885a38
shishc <shishc9@126.com>
2021-05-29 21:27:50 +0800

    test

---
+++ 5-29/src/third1.java
@@ -0,0 +1,9 @@
+/**
+ * @author: ShiShc
+ * @desc: Algorithm Expr3 - 1
+ */
+public class third1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
