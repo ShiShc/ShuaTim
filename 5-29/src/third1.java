8a14f0c79bab0a76157417a8f49d7b53817af25e
shishc <shishc9@126.com>
2021-05-30 17:12:45 +0800

    feat(Expr3) : 1.Knapsack

--- 5-29/src/third1.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author: ShiShc
- * @desc: Algorithm Expr3 - 1
- */
-public class third1 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
