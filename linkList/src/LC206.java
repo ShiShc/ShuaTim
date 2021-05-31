bc51173b6c950c0eee968e2963c2ec3f4f9ea2fb
shishc <shishc9@126.com>
2021-05-31 21:20:57 +0800

    feat : Linkedlist

---
+++ linkList/src/LC206.java
@@ -0,0 +1,22 @@
+/**
+ * @author: ShiShc
+ * @desc:
+ */
+public class LC206 {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = head, cur = pre.next;
+        pre.next = null;
+        while(cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+}
