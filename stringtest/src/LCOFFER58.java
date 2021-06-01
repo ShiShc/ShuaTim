bdd9c106cd63ca9320fd71da4810d9ad18272a08
shishc <shishc9@126.com>
2021-06-01 19:11:44 +0800

    feat : string

---
+++ stringtest/src/LCOFFER58.java
@@ -0,0 +1,9 @@
+/**
+ * @author: ShiShc
+ * @desc:
+ */
+public class LCOFFER58 {
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
+    }
+}
