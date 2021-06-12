817af6123e03ee959267c08255dde500dbecb990
shishc <shishc9@126.com>
2021-06-12 14:21:29 +0800

    greedy

---
+++ greedy2/src/LC435.java
@@ -0,0 +1,13 @@
+/**
+ * @author: ShiShc
+ * @desc:
+ */
+public class LC435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int result = 0;
+
+        
+
+        return result;
+    }
+}
